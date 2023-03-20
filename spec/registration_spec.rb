9b5802c1b0511d97d3f9e30e0ac4c09ead0b6760
OlesiaGnatiuk <olesia.hnatiuk@teladoc.com>
2023-03-20 22:55:13 +0200

    Modified registration

--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -1,16 +1,14 @@
 feature 'Registration', js: true do
 
-  let!(:timestamp) {Time.now.to_i.to_s}
-  let!(:email) {"test" + timestamp + "@gmail.com"}
-  let!(:user_name) {"test" + timestamp}
-
   scenario 'User can register now' do
 
+  user_name = "test#{Time.now.to_i.to_s}"
+  email = "#{user_name}@example.com"
 
     visit('https://gitlab.testautomate.me/')
     find(:xpath, '//*[@id="signin-container"]/p/a').click
-    find('#new_user_first_name').set 'test123g45'
-    find('#new_user_last_name').set 'test12345'
+    find('#new_user_first_name').set 'Test'
+    find('#new_user_last_name').set 'User'
     find('#new_user_username').set user_name
     find('#new_user_email').set email
     find('#new_user_password').set 'test7123456'
@@ -19,12 +17,12 @@ feature 'Registration', js: true do
     select('Development Team Lead', from: "Role")
     select('A different reason', from: "I'm signing up for GitLab because:")
 
+    find(:xpath, '//*[@id="jobs_to_be_done_other"]').set 'Reason'  if find(:xpath, '//*[@id="jobs_to_be_done_other"]').visible?
     find('.gl-button.w-100').click
-
 
     expect(page).to have_content ('Welcome to GitLab')
 
-     sleep 5
+    sleep 5
 
     end
 end
