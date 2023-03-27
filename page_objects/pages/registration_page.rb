2f1a7bd402749d5e49a6564c7c288bbe1c9b1e52
OlesiaGnatiuk <olesia.hnatiuk@teladoc.com>
2023-03-27 23:09:29 +0300

    page object for registration modified

--- page_objects/pages/registration_page.rb
+++ page_objects/pages/registration_page.rb
@@ -8,9 +8,6 @@ class RegistrationPage < SitePrism::Page
   element :password, '#new_user_password'
   element :registration_button, '.btn.gl-button.gl-w-full'
 
- # element :role, :dqs, 'role_dropdown'
- # element :reason, '#user_registration_objective'
- # element :reason_optional, '#jobs_to_be_done_other'
 
   element :get_started_button, '.gl-button.w-100'
 end
